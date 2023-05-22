dc5b002bd07c166de53ba668ff7b3ae37f7c80c3
lehsousa <leandrosousa.work@gmail.com>
2023-05-22 13:27:28 -0300

    first commit

---
+++ todo/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isComplete: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isComplete: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isComplete: false,
+    },
+  ]);
+
+  return (<div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}</p>
+            <p className="category">({todo.category})</p>
+          </div>
+          <div>
+            <button>Completar</button>
+            <button>x</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+  );
+}
+
+export default App;
